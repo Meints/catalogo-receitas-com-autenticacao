97a05a8d148166ef85ec24380711e0a55d31213c
Meints <cadumeints0@gmail.com>
2024-11-07 18:01:24 -0300

    initial css configuration

---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    :focus {
+        outline: 0;
+        box-shadow: 0 0 0 3px ${(props) => props.theme['green-500']};
+    }
+
+    body {
+        background-color: ${(props) => props.theme['gray-100']};
+        color: ${(props) => props.theme['gray-800']};
+        -webkit-font-smoothing: antialiased;
+    }
+
+    border-style, input-security, textarea, button {
+        font-family: 'Roboto', sans-serif;
+        font-size: 1rem;
+        font-weight: 400;
+    }
+`
---
+++ frontend/src/styles/themes/default.ts
@@ -0,0 +1,27 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'blue-100': '#C8DDE5',
+  'blue-300': '#72AFC1',
+  'blue-500': '#16446A',
+  'blue-700': '#0A082D',
+
+  'green-300': '#5CB3A6',
+  'green-500': '#39968A',
+  'green-700': '#256B63',
+
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+
+  'yellow-500': '#FBA94C',
+  'purple-500': '#5B3A8A',
+}
